4de532de4e363deca9a0aeb9de824b5447a4d1f2
Arafullah Shihab <arafullah.shihab@aptos.com>
2024-04-20 05:46:30 -0400

    added routes

--- journal/views.py
+++ journal/views.py
@@ -3,16 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
 
-    pass
+   return render(request, 'journal/index.html')
 
 def register(request):
 
-    pass
+    return render(request, 'journal/register.html')
 
 def my_login(request):
 
-    pass
+    return render(request, 'journal/my-login.html')
 
 def dashboard(request):
 
-    pass
+    return render(request, 'journal/dashboard.html')
